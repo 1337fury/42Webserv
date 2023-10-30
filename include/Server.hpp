45a9e4abba5539cc01593fbc7fbee709645cba13
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-10-30 18:19:00 +0100

    Update Server.hpp with new member functions and settersUpdate Server.hpp with new member functions, setters, and operators

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 12:45:55 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/10/28 12:52:16 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/10/30 15:43:25 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ struct RouteConfig {
     std::string 				cgiExtension;
     bool 						acceptUploads;
     std::string 				uploadDirectory;
-};
+};      
 
 class Server
 {
@@ -35,11 +35,36 @@ class Server
 		int 						_port;
 		std::vector<std::string> 	_serverNames;
 		std::string 				_defaultErrorPage;
-		int 						_clientBodySizeLimit;
+		unsigned long 				_clientBodySizeLimit;
 		std::vector<RouteConfig> 	_routes;
-
+		struct sockaddr_in 			_server_address;
+        int     					_listen_fd;
+		
 	public:
 	// Constructors
 		Server( void );
-		~Server( void );	
+		~Server( void );
+	// Member functions
+	void	parseConfig( std::vector<t_token> tokens );
+	
+	// Getters
+	std::string 				getHost( void ) const;
+	int 						getPort( void ) const;
+	std::vector<std::string> 	getServerNames( void ) const;
+	std::string 				getDefaultErrorPage( void ) const;
+	int 						getClientBodySizeLimit( void ) const;
+	std::vector<RouteConfig> 	getRoutes( void ) const;
+	
+	// Setters
+	void	setHost( std::string host );
+	void	setPort( int port );
+	void	setServerNames( std::vector<std::string> serverNames );
+	void	setDefaultErrorPage( std::string defaultErrorPage );
+	void	setClientBodySizeLimit( int clientBodySizeLimit );
+	void	setRoutes( std::vector<RouteConfig> routes );
+	
+	// Operators
+	Server &operator=( Server const &rhs );
+	Server( Server const &rhs );
+	
 };
