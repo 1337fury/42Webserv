5b8c2f6c6c288a10e2f4ad5baa59a78062041a41
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-16 10:15:11 +0100

    Refactor Server class properties and methods

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 12:45:55 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/10/31 18:47:43 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/11/15 13:16:59 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,17 @@ class Server
 {
 	private:
 	// proberties
-		std::string 				_host;
-		int 						_port;
-		std::vector<std::string> 	_serverNames;
-		std::string 				_defaultErrorPage;
-		unsigned long 				_clientBodySizeLimit;
-		std::vector<RouteConfig> 	_routes;
-		struct sockaddr_in 			_server_address;
-        int     					_listen_fd;
+		in_addr_t 						_host;
+		uint16_t 						_port;
+		std::vector<std::string> 		_serverNames;
+		unsigned long 					_clientBodySizeLimit;
+		std::vector<RouteConfig> 		_routes;
+		std::string 					_root;
+		std::string 					_index;
+		bool 							_autoindex;
+		// struct sockaddr_in 			_server_address;
+		std::map<short, std::string>	_error_pages;
+        int     						_listen_fd;
 		
 	public:
 	// Constructors
@@ -46,26 +49,38 @@ class Server
 		~Server( void );
 	
 	// Getters
-	std::string 				getHost( void ) const;
-	int 						getPort( void ) const;
-	std::vector<std::string> 	getServerNames( void ) const;
-	std::string 				getDefaultErrorPage( void ) const;
-	int 						getClientBodySizeLimit( void ) const;
-	std::vector<RouteConfig> 	getRoutes( void ) const;
-	RouteConfig					getRoute( int index ) const;
-	
+	in_addr_t						getHost( void ) const;
+	int 							getPort( void ) const;
+	std::vector<std::string> 		getServerNames( void ) const;
+	int 							getClientBodySizeLimit( void ) const;
+	std::vector<RouteConfig> 		getRoutes( void ) const;
+	RouteConfig						getRoute( int index ) const;
+	std::string 					getRoot( void ) const;
+	std::string 					getIndex( void ) const;
+	bool 							getAutoindex( void ) const;
+	int 							getListenFd( void ) const;
+	std::map<short, std::string>	getErrorPages( void ) const;
+	std::string 					getErrorPage( short number ) const;
 	
 	// Setters
-	void	setHost( std::string host );
-	void	setPort( int port );
-	void	setServerNames( std::vector<std::string> serverNames );
-	void	setDefaultErrorPage( std::string defaultErrorPage );
-	void	setClientBodySizeLimit( int clientBodySizeLimit );
-	void	setRoutes( std::vector<RouteConfig> routes );
-	void	setRoute( RouteConfig route );
+	void	setHost( std::string );
+	void	setPort( std::string );
+	void	setServerNames( std::vector<std::string> );
+	void	setDefaultErrorPage( std::string );
+	void	setClientBodySizeLimit( std::string );
+	void	setRoutes( std::vector<RouteConfig> );
+	void	setRoute( RouteConfig );
+	void	setRoot( std::string );
+	void	setIndex( std::string );
+	void	setAutoindex( bool );
+	void	setErrorPage( std::vector<std::string> );
+	void	setListenFd( int );
 	
 	// Operators
 	Server &operator=( Server const &rhs );
 	Server( Server const &rhs );
 	
+
+	// to be deleted
+	void printErrorPages() ;
 };
