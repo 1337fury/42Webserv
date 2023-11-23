761bd2ff54741fe8ebc88f3615fa719995ee540e
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-23 19:33:07 +0100

    Add Http class with server initialization and run methods

---
+++ include/Http.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Http.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 12:49:21 by abdeel-o          #+#    #+#             */
+/*   Updated: 2023/11/19 12:51:38 by abdeel-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Server.hpp"
+#include "Webserv.hpp"
+
+class Http
+{
+	private:
+	// Properties
+		std::vector<Server> 	_servers;
+		fd_set					_read_set;
+		fd_set					_write_set;
+		int						_max_fd;
+		std::map<int, Server>	_fd_server_map;
+	// Methods
+		
+		Http( void );
+	public:
+	// Constructors & Destructors
+		Http( std::vector<Server> servers );
+		~Http( void );
+	// Mtethods
+		void	initServers( void );
+		void	run( void );
+		void 	addFDToSet( int fd, fd_set *set );
+};
