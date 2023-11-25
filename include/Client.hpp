08234c61380ae6131bd20253ebe166ee3dbca946
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-25 10:40:25 +0100

    Add Client.hpp file

---
+++ include/Client.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 11:43:53 by abdeel-o          #+#    #+#             */
+/*   Updated: 2023/11/24 11:47:08 by abdeel-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Webserv.hpp"
+#include "Server.hpp"
+
+class Client
+{
+	private:
+
+	public:
+	// Constructors & Destructors
+		Client( void );
+		~Client( void );
+};
