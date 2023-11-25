e801b7a314c75741278a21701c0a0345d4046138
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-25 10:40:51 +0100

    Add Response class to project

---
+++ include/Response.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Response.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 11:47:23 by abdeel-o          #+#    #+#             */
+/*   Updated: 2023/11/24 11:48:24 by abdeel-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Webserv.hpp"
+#include "Server.hpp"
+
+class Response
+{
+	private:
+		
+	public:
+	// Constructors & Destructors
+		Response( void );
+		~Response( void );
+};
