d0d34346dbc13810b2797fd4e14e37e967015742
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-25 10:42:23 +0100

    Initialize Http class with timeout and file descriptor sets

--- srcs/core/Http.cpp
+++ srcs/core/Http.cpp
@@ -19,6 +19,13 @@ Http::Http( void )
 }
 Http::Http( std::vector<Server> servers ) : _servers(servers)
 {
+	_timeout.tv_sec = 1;
+	_timeout.tv_usec = 500000;
+	FD_ZERO(&_read_set);
+	FD_ZERO(&_write_set);
+	_max_fd = 0;
+	FD_ZERO(&_read_set_copy);
+	FD_ZERO(&_write_set_copy);
 }
 Http::~Http( void )
 {
@@ -27,8 +34,6 @@ Http::~Http( void )
 void	Http::initServers( void )
 {
 	Logger::getInstance().log("Initializing servers...", 2);
-	FD_ZERO(&_read_set);
-	FD_ZERO(&_write_set);
 	for (u_short i = 0; i < _servers.size(); i++)
 	{
 		_servers[i].init();
@@ -38,11 +43,44 @@ void	Http::initServers( void )
 	_max_fd = _servers[_servers.size() - 1].getListenFd();
 	/*[Debug]*/ std::cout << "max_fd: " << _max_fd << std::endl;
 }
+
+void   Http::acceptConnection( int listen_fd )
+{
+	struct sockaddr_in  client_addr;
+	socklen_t           client_addr_len = sizeof(client_addr);
+	int                 client_fd;
+
+	client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+	if (client_fd == -1)
+		throw std::runtime_error("accept() failed");
+	Logger::getInstance().log("New connection accepted", 2);
+	addFDToSet(client_fd, &_read_set);
+}
+
 void	Http::run( void )
 {
 	Logger::getInstance().log("Running servers...", 2);
 	while (true)
-		; // TODO: to be continued...
+	{
+		_read_set_copy = _read_set;
+		_write_set_copy = _write_set;
+		if (select(_max_fd + 1, &_read_set_copy, &_write_set_copy, NULL, &_timeout) == -1)
+			throw std::runtime_error("select() failed");
+		for (int i = 0; i <= _max_fd; i++)
+		{
+			if (FD_ISSET(i, &_read_set_copy))
+			{
+				if (i == _servers[0].getListenFd())
+					_servers[0].acceptConnection();
+				else
+					_fd_server_map[i].handleRequest(i);
+			}
+			if (FD_ISSET(i, &_write_set_copy))
+			{
+				_fd_server_map[i].handleResponse(i);
+			}
+		}
+	}
 }
 
 void 	Http::addFDToSet( int fd, fd_set *set )
