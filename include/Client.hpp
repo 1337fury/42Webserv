14aee8a860246bac02795924ed0557d637facaeb
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-26 16:53:29 +0100

    Update Client.hpp to include the Request class and add getters/setters for client socket, address, and request

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 11:43:53 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/11/24 11:47:08 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/11/26 16:48:23 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,30 @@
 
 #include "Webserv.hpp"
 #include "Server.hpp"
+#include "Request.hpp"
 
 class Client
 {
 	private:
-
+		SOCKET			_clientSock;
+		sockaddr_in		_clientAddr;
+		socklen_t		_addrLen;
+		Request			_request; //! [NOT VERIFIED]
 	public:
 	// Constructors & Destructors
 		Client( void );
+		Client( SOCKET, sockaddr_in );
 		~Client( void );
+		Client( const Client & src );
+		Client & operator=( const Client & rhs );
+	// Getters
+		SOCKET			getClientSock( void ) const;
+		sockaddr_in		getClientAddr( void ) const;
+		socklen_t		getClientAddrLen( void ) const;
+		Request			getRequest( void ) const; //! [NOT VERIFIED]
+	// Setters
+		void			setClientSock( SOCKET );
+		void			setClientAddr( sockaddr_in );
+		void			setClientAddrLen( socklen_t );
+		void			setRequest( Request& ); //! [NOT VERIFIED]
 };
