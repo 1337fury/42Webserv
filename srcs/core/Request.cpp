6d068fea7c4acb6aa4836e3127bde2d0b899c970
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-26 16:57:31 +0100

    Update Request.cpp with constructors, assignment operator, and destructor

--- srcs/core/Request.cpp
+++ srcs/core/Request.cpp
@@ -5,8 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/24 11:48:10 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/11/24 11:48:11 by abdeel-o         ###   ########.fr       */
+/*   Created: 2023/11/26 16:28:11 by abdeel-o          #+#    #+#             */
+/*   Updated: 2023/11/26 16:39:27 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Request.hpp"
+
+Request::Request( void ) {
+
+}
+
+Request::Request( Request const &rhs ) {
+	*this = rhs;
+}
+
+Request &Request::operator=( Request const &rhs ) {
+	if ( this != &rhs ) {
+	}
+	return *this;
+}
+
+Request::~Request( void ) {
+}
+
+// Getters
