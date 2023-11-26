f5120b3d7960f7f0fdc45a52efb992562b74440e
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-26 16:53:52 +0100

    Update Logger class to include a variadic log method

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 12:58:29 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/11/23 13:00:54 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/11/25 16:21:23 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 #include <sstream>
 #include <iomanip>
 #include <ctime>
+#include <stdarg.h>
 
+#define BUFFER_SIZE 4000
 // Singleton class : is a class that can have only one object (an instance of the class) at a time.
 class Logger 
 {
 	public:
 		static Logger& getInstance();
-		void log(const std::string& message, int colorCode);
+		void log(int colorCode, const char *format, ...);
 
 	private:
 		Logger() {}
