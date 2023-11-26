e26f3c4f91076c2d93728fc7f350a2ff1d9a78bc
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-26 16:58:19 +0100

    Update server.cpp with changes to include additional header files and add new functions for accepting connections and handling requests

--- srcs/core/server.cpp
+++ srcs/core/server.cpp
@@ -6,12 +6,14 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 12:52:40 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/11/23 15:48:40 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/11/26 16:49:52 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 #include "Logger.hpp"
+#include "Http.hpp"
+#include "Client.hpp"
 
 Server::Server( void ) {
 	this->_host = 0;
@@ -159,12 +161,12 @@ void Server::printErrorPages()
 void	Server::init( void ) {
 	this->_listen_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (!ISVALIDSOCKET(this->_listen_socket)) {
-		Logger::getInstance().log("WebServ: Socket creation failed", 1);
+		Logger::getInstance().log(COLOR_RED, "WebServ: Socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 	int opt = 1;
 	if (setsockopt(this->_listen_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-		Logger::getInstance().log("WebServ: Socket options failed", 1);
+		Logger::getInstance().log(COLOR_RED, "WebServ: Socket options failed");
 		exit(EXIT_FAILURE);
 	}
 	memset(&_server_address, 0, sizeof(_server_address));
@@ -172,15 +174,91 @@ void	Server::init( void ) {
 	_server_address.sin_addr.s_addr = this->_host;
 	_server_address.sin_port = htons(this->_port); // htons() converts the port number to network byte order
 	if (bind(this->_listen_socket, (struct sockaddr *)&_server_address, sizeof(_server_address)) == -1) {
-		Logger::getInstance().log("WebServ: Socket binding failed", 1);
+		Logger::getInstance().log(COLOR_RED, "WebServ: Socket binding failed");
 		exit(EXIT_FAILURE);
 	}
 	if (listen(this->_listen_socket, 512) == -1) {
-		Logger::getInstance().log("WebServ: Socket listening failed", 1);
+		Logger::getInstance().log(COLOR_RED, "WebServ: Socket listening failed");
 		exit(EXIT_FAILURE);
 	}
-	if (set_non_blocking(this->_listen_socket) == -1) {
-		Logger::getInstance().log("WebServ: Socket non-blocking failed", 1);
+	// if (set_non_blocking(this->_listen_socket) == -1) {
+	// 	Logger::getInstance().log(COLOR_RED, "WebServ: Socket non-blocking failed");
+	// 	exit(EXIT_FAILURE);
+	// } //? what Happens if we don't set the client socket to non-blocking? : we will have to wait for the client to send data before we can send data to it
+}
+
+void   Server::acceptConnection( fd_set &read_set)
+{
+	sockaddr_in  client_addr; // create a structure to hold the address of the client that will connect using the socket
+	socklen_t           client_addr_len = sizeof(client_addr); // get the size of the structure
+	int                 client_fd; // create a file descriptor for the client socket
+
+	/*[debug]*/for (int i = 0; i <= 9; i++)
+		{
+			if (FD_ISSET(i, &Http::read_set))
+				std::cout << "read set[accept]: " << i << std::endl;
+		}
+
+	client_fd = accept(_listen_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+	if (!ISVALIDSOCKET(client_fd))
+	{
+		Logger::getInstance().log(COLOR_RED, "WebServ: Socket accept failed");
 		exit(EXIT_FAILURE);
 	}
+	/*[debug]*/std::cout << "client_fd [accept]: " << client_fd << std::endl;
+	Logger::getInstance().log(COLOR_GREEN, "A connection to `%s` has been established from %s", _serverNames[0].c_str() ,inet_ntoa(client_addr.sin_addr));
+
+	Http::addFDToSet(client_fd, &read_set); //? Can be better
+	
+	// if (set_non_blocking(client_fd) == -1)
+	// {
+	// 	Logger::getInstance().log(COLOR_RED, "WebServ: Socket non-blocking failed");
+	// 	exit(EXIT_FAILURE);
+	// } //? what Happens if we don't set the client socket to non-blocking? : we will have to wait for the client to send data before we can send data to it
+
+	Client client(client_fd, client_addr);
+	client.setClientAddrLen(client_addr_len);
+
+	if (Http::fd_client_map.count(client_fd) == 1)
+		// Http::fd_client_map[client_fd] = client; //! To be checked
+		return ;
+	else
+		Http::fd_client_map.insert(std::pair<SOCKET, Client>(client_fd, client));
+}
+
+void	Server::handleRequest( __unused int fd ) {
+		/*[debug]*/std::cout << "client_fd: " << fd << std::endl;
+		/*[debug]*/for (int i = 0; i <= 9; i++)
+		{
+			if (FD_ISSET(i, &Http::read_set))
+				std::cout << "read set: " << i << std::endl;
+		}
+	Logger::getInstance().log(COLOR_YELLOW, "Handling request...");
+	char buffer[REQUEST_BUFFER_SIZE];
+	int bytes_received = 0;
+	bytes_received = recv(fd, buffer, REQUEST_BUFFER_SIZE, 0);
+		/*[debug]*/std::cout << "Request " <<  buffer << std::endl;
+	// TODO: Handle request 
+	// - if request is empty close connection because client closed connection without sending request,it send an ACK to the server and the server recv() returns 0
+	// - if request is not empty parse it
+	// - if request is not valid send error response
+	// - if request is valid send response
+	// - if request is valid and connection is keep-alive wait for another request
+	// - if request is valid and connection is not keep-alive close connection
+	if (bytes_received == 0)
+	{
+		Logger::getInstance().log(COLOR_YELLOW, "Connection closed by peer");
+		Http::closeConnection(fd);
+	}
+	else if (bytes_received == -1)
+	{
+		Logger::getInstance().log(COLOR_RED, "WebServ: Socket recv failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (bytes_received > 0)
+	{
+		//? parse request [...]
+		memset(buffer, 0, REQUEST_BUFFER_SIZE);
+	}
+	//send response
 }
