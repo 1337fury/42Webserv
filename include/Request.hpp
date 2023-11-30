d2ccb1af5c9ddb342cb73455f050d201f282f8c8
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-11-30 17:00:17 +0100

    Update Request.hpp to use vector<char> instead of string for content storage

--- include/Request.hpp
+++ include/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 11:46:52 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/11/28 16:18:15 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/11/28 20:26:27 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,6 @@ class Request
 		int 					versionMajor;
 		int 					versionMinor;
 		std::vector<Header> 	headers;
-		std::vector<char> 		content;
+		std::vector<char> 		content; // we use vector<char> instead of string because string is null terminated and we want to be able to send binary data in the body of the request (like images) and we don't want to stop at the first null character we encounter in the body of the request (if we use string) so we use vector<char> instead, image binary data example: \x89\x50\x4E\x47\x0D\x0A\x1A\x0A so we use vector<char> to be able to store \x00
 		bool 					keepAlive;
 };
