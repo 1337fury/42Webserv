7055922cae317b63969c64023173986034326486
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-12-02 17:36:08 +0100

    Update Request.hpp with operator<< function for printing request prettily

--- include/Request.hpp
+++ include/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 11:46:52 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/11/28 20:26:27 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/11/30 17:39:55 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,3 +47,5 @@ class Request
 		std::vector<char> 		content; // we use vector<char> instead of string because string is null terminated and we want to be able to send binary data in the body of the request (like images) and we don't want to stop at the first null character we encounter in the body of the request (if we use string) so we use vector<char> instead, image binary data example: \x89\x50\x4E\x47\x0D\x0A\x1A\x0A so we use vector<char> to be able to store \x00
 		bool 					keepAlive;
 };
+
+std::ostream &operator<<( std::ostream &o, Request const &rhs );// we use this function to print the request in a pretty way
