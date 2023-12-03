ba8845bd357ecbd0d495996a1d8786752ccdd384
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-12-03 19:29:10 +0100

    Update Request.cpp with ostream operator<< for Request class

--- srcs/core/Request.cpp
+++ srcs/core/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 16:28:11 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/11/26 16:39:27 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/11/30 18:18:28 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,18 @@ Request &Request::operator=( Request const &rhs ) {
 Request::~Request( void ) {
 }
 
-// Getters
+std::ostream &operator<<( std::ostream &o, Request const &i ) {
+	o << "Print Request:" << std::endl;
+	o << "\tMethod: " << i.method << std::endl;
+	o << "\tUri: " << i.uri << std::endl;
+	o << "\tHttpVersion: " << i.versionMajor << "/" << i.versionMinor << std::endl;
+	o << "\tHeaders: " << std::endl;
+	for (size_t j = 0; j < i.headers.size(); j++)
+		o << "\t\t" << i.headers[j].key << ": " << i.headers[j].value << std::endl;
+	o << "\tContent: " << std::endl;
+	for (size_t j = 0; j < i.content.size(); j++)
+		o << i.content[j];
+	o << std::endl;
+	o << "\tKeepAlive: " << i.keepAlive << std::endl;
+	return o;
+}
