26c020b33049bc50005997f9236361b2f3ff9074
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-12-03 19:28:14 +0100

    Update Response.cpp with constructors and destructors

--- srcs/core/Response.cpp
+++ srcs/core/Response.cpp
@@ -6,7 +6,21 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 11:48:07 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/11/24 11:48:08 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/12/02 18:23:27 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Response.hpp"
+
+// Constructors & Destructors
+Response::Response( void )
+{
+	_status = "200 OK";
+	_headers.push_back(Header("Server", "Nginy/1.0"));
+	_content = std::vector<char>();
+}
+
+Response::~Response( void )
+{
+}
+// Methods
