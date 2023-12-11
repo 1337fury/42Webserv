ff7aad3a3660f6f45f56105964e31f68515d85aa
Abdelhalim El Ouardy <elouardyabdelhalim@gmail.com>
2023-12-11 18:31:50 +0100

    Update Response.cpp with added functionality for handling HTTP GET requests and sending appropriate responses

--- srcs/core/Response.cpp
+++ srcs/core/Response.cpp
@@ -6,27 +6,32 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 11:48:07 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/12/05 12:38:53 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/12/11 17:20:54 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Response.hpp"
+#include "Client.hpp"
+#include "Http.hpp"
+#include "Logger.hpp"
 
 // Constructors & Destructors
 Response::Response( void )
 {
 	_statusCode = 200;
 	_status = "OK";
-	// _headers.push_back(Header("Server", "Nginy/1.0"));
 	_content = std::vector<char>();
 }
 
-Response::Response( Request &request ) : _request(request)
+Response::Response( Request &request, Server &server ) : 
+_server(server),
+_request(request)
 {
 	_statusCode = 200;
 	_status = "OK";
-	// _headers.push_back(Header("Server", "Nginy/1.0"));
 	_content = std::vector<char>();
+	_keepAlive = false;
+	_location = _server.getMatchingLocation(_request.uri);
 }
 
 Response::~Response( void )
@@ -56,7 +61,7 @@ Response &Response::operator=( Response const &rhs )
 }
 
 // Getters
-unsigned int			Response::getStatusCode( void ) const
+u_short			Response::getStatusCode( void ) const
 {
 	return _statusCode;
 }
@@ -78,7 +83,7 @@ Header					Response::getHeader( std::string key ) const
 		if ( it->key == key )
 			return *it;
 	}
-	return Header();
+	return Header("", "");
 }
 
 std::vector<char> 		Response::getContent( void ) const
@@ -112,7 +117,7 @@ std::string				Response::getResponseString( void ) const
 }
 
 // Setters
-void					Response::setStatusCode( unsigned int statusCode )
+void					Response::setStatusCode( u_short statusCode )
 {
 	_statusCode = statusCode;
 }
@@ -129,7 +134,7 @@ void					Response::setHeaders( std::vector<Header> headers )
 
 void					Response::setHeader( std::string key, std::string value  )
 {
-	Header header;
+	Header header("", "");
 	header.key = key;
 	header.value = value;
 	_headers.push_back(header);
@@ -169,8 +174,148 @@ void					Response::setResponseString( void )
 	_response_string += _body;
 }
 
-// Methods
-void	Response::create( void )
+std::string Response::getCurrentTime() {
+    time_t currentTime = time(nullptr);
+    char buffer[50]; 
+
+   	strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S GMT", gmtime(&currentTime));
+   	return  std::string(buffer);
+}
+
+void					Response::init_headers( void )
 {
+	_headers.push_back(Header("Server", "Nginy/1.0"));
+	_headers.push_back(Header("Date", getCurrentTime()));
+	_headers.push_back(Header("Last-Modified", getCurrentTime()));
+	_headers.push_back(Header("Connection", _keepAlive ? "keep-alive" : "close"));
+	if ( _keepAlive )
+		_headers.push_back(Header("Keep-Alive", "timeout=5, max=1000")); // max=1000 means that the client can send 1000 requests before the connection is closed
+}
+
+// Methods
+void Response::searchForErrorPage( void )
+{
+	std::string error_page = _server.getErrorPage(_statusCode);
+	if ( error_page != "" )
+	{
+		std::ifstream file(error_page);
+		if ( file.is_open() )
+		{
+			std::string line;
+			while ( getline(file, line) )
+				_page << line;
+			file.close();
+			_body = _page.str();
+			_content = std::vector<char>(_body.begin(), _body.end());
+		}
+		else
+		{
+			_statusCode = 500;
+			_status = "Internal Server Error";
+			_body = "<html><body><h1>500 Internal Server Error</h1></body></html>";
+			_content = std::vector<char>(_body.begin(), _body.end());
+		}
+	}
+	else
+	{
+		_statusCode = 500;
+		_status = "Internal Server Error";
+		_body = "<html><body><h1>500 Internal Server Error</h1></body></html>";
+		_content = std::vector<char>(_body.begin(), _body.end());
+	}
+}
+
+reqStatus	Response::analyzeRequest( void )
+{
+	std::string path = _request.uri;
 	
+	if (!_location)
+		return LOCATION_NOT_FOUND;
+	return OK;
+}
+
+void	Response::create( __unused Client& client )
+{
+	reqStatus requestStatus = analyzeRequest();
+	switch (requestStatus)
+	{
+		case LOCATION_NOT_FOUND:
+			sendMethodNotAllowedResponse(client.getClientSock(), 404);
+			break;
+		default:
+			break;
+	}
+}
+
+/*void	Response::create( Client& client )
+{
+	analyzeRequest();
+	if ( _request.method == "GET" )
+		goto getResponse;
+	else if ( _request.method == "POST" )
+		goto postResponse;
+	else if ( _request.method == "DELETE" )
+		goto deleteResponse;
+	else
+		goto methodNotAllowedResponse;
+
+	getResponse:
+		handleGETrequest(client.getClientSock());
+	postResponse:
+		return ;
+	deleteResponse:
+		return ;
+	methodNotAllowedResponse:
+		sendMethodNotAllowedResponse(client.getClientSock());
+}*/
+
+void	Response::reset( void )
+{
+	_statusCode = 200;
+	_status = "OK";
+	_headers.clear();
+	_content.clear();
+	_request = Request();
+	_keepAlive = false;
+	_version = "";
+	_body = "";
+	_response_string = "";
+}
+
+// response
+void	Response::sendMethodNotAllowedResponse( SOCKET clientSock, u_short statusCode )
+{
+	Logger::getInstance().log(COLOR_CYAN, "Sending Method Not Allowed Response");
+	setVersion(1, 1);
+	setStatusCode(statusCode);
+	setStatusMessage(status_code(statusCode));
+	setKeepAlive(false);
+	init_headers();	
+	searchForErrorPage();
+	setBody();
+	setResponseString();
+	send(clientSock, _response_string.c_str(), _response_string.length(), 0);
+	Http::closeConnection(clientSock);
+	Http::removeFDFromSet(clientSock, &Http::write_set);
+}
+
+// Example for an HTTP GET request:
+// GET /static HTTP/1.1
+// Host: www.example.com
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)
+// Chrome/87.0.4280.88 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,
+// image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+
+// process request and send appropriate response
+void	Response::handleGETrequest( __unused SOCKET clientSock )
+{
+	Logger::getInstance().log(COLOR_CYAN, "Responding to GET request...");
+	std::string uri = _request.uri;
+	
+	Http::closeConnection(clientSock);
+	Http::removeFDFromSet(clientSock, &Http::write_set);
 }
